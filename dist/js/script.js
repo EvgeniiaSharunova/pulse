ec4a690bf290db4ec271d8fa022a5126f9049cfd
Evgeniia <jeka.sharunova@gmail.com>
2023-05-06 02:07:22 +0700

    Add files optimization

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         /* adaptiveHeight: true, */
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/chevron-left-solid.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/chevron-right-solid.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/chevron-left-solid.svg" alt="chevron left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/chevron-right-solid.svg" alt="chevron right"></button>',
         responsive: [
             {
                 breakpoint: 992,
